5c52c852a244e1cf9fa778095b2e99168dfcdb74
acristoni <adonai_cn@hotmail.com>
2023-09-14 08:59:25 -0300

    REFACT: search data fucntion

--- src/utils/searchData.tsx
+++ src/utils/searchData.tsx
@@ -5,11 +5,30 @@ export default function searchData(searchWord: string, setAnimals: (value: Anima
     const allAnimals = mock.mockData;
 
     const animalsTypeFound = allAnimals.filter(animalInfo => animalInfo.type.toLowerCase() === searchWord.toLowerCase())
+    
     const animalTitleFound = allAnimals.filter(animalInfo => {
       const arrayWordsTitle = animalInfo.title.split(' ')
       const arrayWordsTitleLowerCase = arrayWordsTitle.map((word: string) => word.toLowerCase())
       return arrayWordsTitleLowerCase.includes(searchWord.toLowerCase())
     })
 
-    setAnimals(animalTitleFound.length ? animalTitleFound : animalsTypeFound);
+    const setDataAnimals = new Set([...animalsTypeFound, ...animalTitleFound])
+    const arrayNoDuplicate = [...setDataAnimals]
+    const arraySorted = arrayNoDuplicate.sort(compareByTitle)
+
+    setAnimals(arraySorted);
+}
+
+function compareByTitle(a: Animal, b: Animal) {
+  const titleA = a.title.toUpperCase();
+  const titleB = b.title.toUpperCase();
+
+  if (titleA < titleB) {
+    return -1;
+  }
+  if (titleA > titleB) {
+    return 1;
+  }
+
+  return 0;
 }
