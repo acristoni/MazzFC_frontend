f4f39a2dc255e6e8ecc170fb31331508abf5cb7c
acristoni <adonai_cn@hotmail.com>
2023-09-14 08:28:08 -0300

    FEAT: hover result

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import { Animal } from "../interfaces/animal.interface";
 
 export default function Result({ info }: { info: Animal }) {
+    const [isHover, setIsHover] = useState<boolean>(false)
+
     return (
-        <div className="Result">
+        <div 
+            className="Result" 
+            onMouseEnter={()=>setIsHover(true)} 
+            onMouseLeave={()=>setIsHover(false)}
+        >
             <p>{info.url}</p>
-            <p className="Title-result">{info.title}</p>
+            <p 
+                className="Title-result" 
+                style={{ textDecoration: isHover ? 'underline' : 'none'}} 
+            >{info.title}</p>
             <p style={{ textAlign: 'start' }}>{info.description}</p>
         </div>
     )
